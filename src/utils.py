32de53e6fe32d6b8068e9025a9e4fd17fe5721d2
sandra-212 <a.pechenina@gmail.com>
2025-08-13 10:49:12 -0400

    update

--- src/utils.py
+++ src/utils.py
@@ -12,7 +12,6 @@ def parse_xml_files(folder: str = "data") -> dict:
     for file_name in os.listdir(folder):
         file_path = os.path.join(folder, file_name)
 
-        # Only process XML files
         if os.path.isfile(file_path) and file_name.lower().endswith(".xml"):
             try:
                 with open(file_path, "r", encoding="utf-8") as f:
@@ -20,7 +19,7 @@ def parse_xml_files(folder: str = "data") -> dict:
                     parsed = xmltodict.parse(xml_data)
                     first_key = next(iter(parsed))
                     parsed_data= parsed[first_key]
-                    # parsed_data = data.get("AirFareSearchResponse", {})
+
             except Exception as e:
                 print(f"Failed to parse : {e}")
 
